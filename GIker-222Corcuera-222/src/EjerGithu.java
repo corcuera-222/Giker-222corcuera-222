70c8d9bbdb2c168e2dd12e843a635ca5acffcd25
IKER <IKER@172.31.54.84>
2022-04-27 11:28:18 +0200

    update

---
+++ GIker-222Corcuera-222/src/EjerGithu.java
@@ -0,0 +1,46 @@
+
+public class EjerGithu
+ {
+
+	public static void main(String[] args)
+	{
+		
+				
+				//declaracion
+				byte numero;
+				short numeroShort;
+				int numeroEntero;
+				long numeroLargo;
+				float numeroComas;
+				double numeroDouble;
+				char caracter;
+				boolean bo;
+				// asignacion
+				numero=3;
+				numeroShort=200;
+				numeroEntero=4800;
+				numeroLargo=7843L;
+				numeroComas=1.78F;
+				numeroDouble=17.89D;
+				caracter='?';
+				bo=false;
+				//visualizar valores
+				System.out.print("los valores de las variables son los siguientes:");
+				System.out.print("\n"+numero+"\n"+numeroShort+"\n"+numeroEntero+"\n"+numeroLargo+"\n"+numeroComas+"\n"+numeroDouble+"\n"+caracter+"\n"+bo);
+						
+			
+				
+				
+				
+				
+				
+
+			}
+
+		
+
+
+
+	}
+
+
